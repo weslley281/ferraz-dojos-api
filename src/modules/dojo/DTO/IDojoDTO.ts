4f6549cd92c2fb7013c90082b3ccce25174cc8a8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-04-07 20:51:05 -0400

    a

---
+++ src/modules/dojo/DTO/IDojoDTO.ts
@@ -0,0 +1,15 @@
+interface ICreateDojoDTO {
+  id_dojo?: string;
+  dojo: string;
+  password?: string;
+  address_line1: string;
+  address_line2: string;
+  city: string;
+  state: string;
+  country: string;
+  phone: string;
+  email?: string;
+  paid_out?: boolean;
+  updatedAt?: Date;
+  created_at?: Date;
+}
