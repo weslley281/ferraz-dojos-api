c9a8b8892dcd346dac318ec1f1793ec287e7155d
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-04-08 09:53:43 -0400

    a

--- src/routes/dojo.routes.ts
+++ src/routes/dojo.routes.ts
@@ -4,15 +4,15 @@ import { updateDojoController } from '../modules/dojo/useCases/updateDojo';
 import { listDojoController } from '../modules/dojo/useCases/listaAllDojos';
 
 export async function DojosRoutes(app: FastifyInstance) {
+  app.get('/', async (request, responce) => {
+    listDojoController.handle(request, responce);
+  });
+
   app.post('/', async (request, responce) => {
     createDojoController.handle(request, responce);
   });
 
   app.put('/', async (request, responce) => {
     updateDojoController.handle(request, responce);
-  });
-
-  app.get('/all', async (request, responce) => {
-    listDojoController.handle(request, responce);
   });
 }
